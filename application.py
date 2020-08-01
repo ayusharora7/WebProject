84853cd1cec5d546fe6fea156d1d34da0e7d6d48
ayusharora7 <50331990+ayusharora7@users.noreply.github.com>
2020-08-01 23:50:36 +0530

    Update application.py

--- application.py
+++ application.py
@@ -12,7 +12,6 @@ from sqlalchemy import MetaData
 
 
 app = Flask(__name__)
-DATABASE_URL="postgres://uobuenrgremqke:3d1075ad33333c4f0360b06eb937197d1b3fea01c1d6014399da3588e83fd2f8@ec2-3-216-129-140.compute-1.amazonaws.com:5432/d4a61o8mdgkfd9"
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
